d3988700a7156791f0fd43b3a9d99e4b2684c70d
Karunakar Bandikatla <karunakar.bandikatla@cigna.com>
2018-01-11 16:00:59 +0530

    Initial Commit

---
+++ RealmDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmDB
+//
+//  Created by Karunakar Bandikatla on 11/01/18.
+//  Copyright © 2018 Karunakar Bandikatla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
